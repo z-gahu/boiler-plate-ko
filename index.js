c932987530aed316d03a68eb6989383afa1cbca1
khjang <khjang@mail.com>
2022-12-08 01:14:37 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017", {
+  .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
@@ -13,6 +24,18 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!!!!"));
+
+app.post("/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 Client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port  ${port}`));
